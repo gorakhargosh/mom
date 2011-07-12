3a36c67081fad24f83fb4fac76a597a4f8df32d7
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-12 19:57:53 +0530

    Generates odd length byte string.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- tests/test_mom_codec.py
+++ tests/test_mom_codec.py
@@ -20,53 +20,53 @@ from mom.codec import \
 random_bytes_1024 = generate_random_bytes(1024 >> 3)
 # Generates a 2048-bit strength random byte string.
 random_bytes_2048 = generate_random_bytes(2048 >> 3)
-# Generates a 4096-bit strength random byte string.
-random_bytes_4096 = generate_random_bytes(4096 >> 3)
+# Generates a 4093 byte length random byte string.
+random_bytes_len_4093 = generate_random_bytes(4093)
 
 
 class Test_base64_codec(unittest2.TestCase):
     def test_encodes_without_trailing_newline(self):
         self.assertFalse(base64_encode(random_bytes_1024).endswith("\n"))
         self.assertFalse(base64_encode(random_bytes_2048).endswith("\n"))
-        self.assertFalse(base64_encode(random_bytes_4096).endswith("\n"))
+        self.assertFalse(base64_encode(random_bytes_len_4093).endswith("\n"))
 
     def test_codec_identity(self):
         self.assertEqual(base64_decode(base64_encode(random_bytes_1024)), random_bytes_1024)
         self.assertEqual(base64_decode(base64_encode(random_bytes_2048)), random_bytes_2048)
-        self.assertEqual(base64_decode(base64_encode(random_bytes_4096)), random_bytes_4096)
+        self.assertEqual(base64_decode(base64_encode(random_bytes_len_4093)), random_bytes_len_4093)
 
 class Test_base64_codec(unittest2.TestCase):
     def test_encodes_without_trailing_newline(self):
         self.assertFalse(bytes_to_base64(random_bytes_1024).endswith("\n"))
         self.assertFalse(bytes_to_base64(random_bytes_2048).endswith("\n"))
-        self.assertFalse(bytes_to_base64(random_bytes_4096).endswith("\n"))
+        self.assertFalse(bytes_to_base64(random_bytes_len_4093).endswith("\n"))
 
     def test_codec_identity(self):
         self.assertEqual(base64_to_bytes(bytes_to_base64(random_bytes_1024)), random_bytes_1024)
         self.assertEqual(base64_to_bytes(bytes_to_base64(random_bytes_2048)), random_bytes_2048)
-        self.assertEqual(base64_to_bytes(bytes_to_base64(random_bytes_4096)), random_bytes_4096)
+        self.assertEqual(base64_to_bytes(bytes_to_base64(random_bytes_len_4093)), random_bytes_len_4093)
 
 class Test_bytes_hex_codec(unittest2.TestCase):
     def test_codec_identity(self):
         self.assertEqual(hex_to_bytes(bytes_to_hex(random_bytes_1024)), random_bytes_1024)
         self.assertEqual(hex_to_bytes(bytes_to_hex(random_bytes_2048)), random_bytes_2048)
-        self.assertEqual(hex_to_bytes(bytes_to_hex(random_bytes_4096)), random_bytes_4096)
+        self.assertEqual(hex_to_bytes(bytes_to_hex(random_bytes_len_4093)), random_bytes_len_4093)
 
 class Test_bytes_decimal_codec(unittest2.TestCase):
     def test_codec_identity(self):
         self.assertEqual(decimal_to_bytes(bytes_to_decimal(random_bytes_1024)), random_bytes_1024)
         self.assertEqual(decimal_to_bytes(bytes_to_decimal(random_bytes_2048)), random_bytes_2048)
-        self.assertEqual(decimal_to_bytes(bytes_to_decimal(random_bytes_4096)), random_bytes_4096)
+        self.assertEqual(decimal_to_bytes(bytes_to_decimal(random_bytes_len_4093)), random_bytes_len_4093)
 
 class Test_bytes_bin_codec(unittest2.TestCase):
     def test_codec_identity(self):
         self.assertEqual(bin_to_bytes(bytes_to_bin(random_bytes_1024)), random_bytes_1024)
         self.assertEqual(bin_to_bytes(bytes_to_bin(random_bytes_2048)), random_bytes_2048)
-        self.assertEqual(bin_to_bytes(bytes_to_bin(random_bytes_4096)), random_bytes_4096)
+        self.assertEqual(bin_to_bytes(bytes_to_bin(random_bytes_len_4093)), random_bytes_len_4093)
 
 
 class Test_bytes_long_codec(unittest2.TestCase):
     def test_codec_identity(self):
         self.assertEqual(long_to_bytes(bytes_to_long(random_bytes_1024)), random_bytes_1024)
         self.assertEqual(long_to_bytes(bytes_to_long(random_bytes_2048)), random_bytes_2048)
-        self.assertEqual(long_to_bytes(bytes_to_long(random_bytes_4096)), random_bytes_4096)
+        self.assertEqual(long_to_bytes(bytes_to_long(random_bytes_len_4093)), random_bytes_len_4093)
