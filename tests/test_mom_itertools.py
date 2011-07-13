0a611674a4339dcc99f2d5f34a3c87f8a304d112
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-14 05:14:27 +0530

    Adds tests for itertools.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

---
+++ tests/test_mom_itertools.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+import unittest2
+from mom.itertools import group
+
+
+class Test_group(unittest2.TestCase):
+    def test_valid_grouping(self):
+        self.assertEqual(list(group("aaaabbbbccccdddd", 4)), ["aaaa", "bbbb", "cccc", "dddd"])
+        self.assertEqual(list(group([1, 1, 1, 2, 2, 2, 3, 3, 3], 3)), [[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+
+    def test_returns_generator_object(self):
+        self.assertTrue("generator" in repr(type(group("aaaabbbb", 4))))
+
+    def test_odd_ball_grouping(self):
+        self.assertEqual(list(group("aaabb", 3)), ["aaa", "bb"])
