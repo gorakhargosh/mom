5f7806e9f256e2e2fdf38b0031f7029d73548ac6
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-25 07:53:38 +0530

    LCM and GCD tests.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- tests/test_mom_math.py
+++ tests/test_mom_math.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 
 import unittest2
 
-from mom.math import is_prime, _pure_is_prime, \
+from mom.math import gcd, lcm, is_prime, _pure_is_prime, \
     generate_random_prime, generate_random_safe_prime
 from mom._prime_sieve import make_prime_sieve
 
@@ -31,4 +31,18 @@ class Test_generate_random_safe_prime(unittest2.TestCase):
     def test_generate_random_safe_prime(self):
         for x in range(100):
             self.assertTrue(is_prime(generate_random_safe_prime(64)))
+        
+
+class Test_gcd(unittest2.TestCase):
+    def test_gcd(self):
+        self.assertEqual(gcd(54, 24), 6)
+
+    def test_gcd_swap(self):
+        self.assertEqual(gcd(24, 54), 6)
+
+class Test_lcm(unittest2.TestCase):
+    def test_lcm(self):
+        self.assertEqual(lcm(4, 6), 12)
+        self.assertEqual(lcm(6, 4), 12)
+        self.assertEqual(lcm(21, 6), 42)
         
