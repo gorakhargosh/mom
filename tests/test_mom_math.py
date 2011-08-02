dd7586ae9d6c242eb47928d1bfba7bce4907d790
Gora Khargosh <gora.khargosh@gmail.com>
2011-08-02 17:30:50 +0530

    Random safe prime is a very slow test.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- tests/test_mom_math.py
+++ tests/test_mom_math.py
@@ -29,7 +29,7 @@ class Test_generate_random_prime(unittest2.TestCase):
         
 class Test_generate_random_safe_prime(unittest2.TestCase):
     def test_generate_random_safe_prime(self):
-        for x in range(100):
+        for x in range(20):
             self.assertTrue(is_prime(generate_random_safe_prime(64)))
         
 
