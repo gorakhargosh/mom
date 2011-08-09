85ae1671b22414738dd07f8bb1c4adde999e2bc0
Gora Khargosh <gora.khargosh@gmail.com>
2011-08-09 17:01:03 +0530

    Fixes more documentation.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- mom/codec/base58.py
+++ mom/codec/base58.py
@@ -101,7 +101,7 @@ identifier and serve the file fresh as it is on the server file system. The
 hashed-identifier is not part of the query string for this asset URL because
 certain proxies do not cache files served from URLs that include query
 strings. If you wish to support versioned assets, however, then you
-may need to rename files to their hashed identifiers instead.
+may need to rename files to include their hashed identifiers instead.
 
 The following benefits are therefore achieved::
 
