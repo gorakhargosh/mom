52d64bef965de87fba540c76470773d82dae57bc
Gora Khargosh <gora.khargosh@gmail.com>
2011-08-11 03:27:28 +0530

    integer_to_bytes now does a little less calculation.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- mom/codec/integer.py
+++ mom/codec/integer.py
@@ -131,8 +131,13 @@ def _integer_to_bytes(num, chunk_size=0):
 
     # Add back some pad bytes. This could be done more efficiently w.r.t. the
     # de-padding being done above, but sigh...
-    if chunk_size > 0 and len(raw_bytes) % chunk_size:
-        bytes_needed = integer_byte_count(number)
+    length = len(raw_bytes)
+    if chunk_size > 0 and length % chunk_size:
+        zero_leading = 0
+        for zero_leading, x in enumerate(raw_bytes):
+            if x != ZERO_BYTE[0]:
+                break
+        bytes_needed = length - zero_leading
         if bytes_needed > chunk_size:
             raise OverflowError(
                 "Need %d bytes for number, but chunk size is %d" %
@@ -176,12 +181,14 @@ def integer_to_bytes(number, chunk_size=0):
         # Bounds checking. We're not doing this up-front because the
         # most common use case is not specifying a chunk size. In the worst
         # case, the number will already have been converted to bytes above.
-        bytes_needed = integer_byte_count(number)
+        zero_leading = 0
+        for zero_leading, x in enumerate(raw_bytes):
+            if x != ZERO_BYTE[0]:
+                break
+        bytes_needed = length - zero_leading
         if bytes_needed > chunk_size:
-            raise OverflowError(
-                "Need %d bytes for number, but chunk size is %d" %
-                (bytes_needed, chunk_size)
-            )
+            raise OverflowError('Needed %i bytes for number, but block size '
+                'is %i' % (bytes_needed, chunk_size))
         remainder = length % chunk_size
         if remainder:
             raw_bytes = (chunk_size - remainder) * ZERO_BYTE + raw_bytes
