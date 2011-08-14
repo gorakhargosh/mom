c3f957632b16e7c848834b79ab415df16731d823
Gora Khargosh <gora.khargosh@gmail.com>
2011-08-14 18:04:48 +0530

    Adds note about watchdog.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- autotest.sh
+++ autotest.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-
-watchmedo shell-command --patterns="*.idea;*.py;*.c" --recursive --command='tox -e py27' .
+#
+# You will need watchdog installed for this to work.
+watchmedo shell-command \
+    --patterns="*.idea;*.py;*.c" \
+    --recursive \
+    --command='tox -e py27' \
+    .
