62365cfb58c1a9915d5b3feaef31dff4fae5ace9
Gora Khargosh <gora.khargosh@gmail.com>
2011-09-07 02:08:37 +0530

    Absolute imports in one of the tests.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- mom/tests/test_mom_codec_base85.py
+++ mom/tests/test_mom_codec_base85.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+
 import os
 import unittest2
 from mom.builtins import b
