abec41337cd3dc803226d82dd7d3ea6c7488d2b0
Yesudeep Mangalapilly <yesudeep@google.com>
2012-05-28 11:04:57 +0530

    Update copyright lines.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@google.com>

--- tools/dump_primes.py
+++ tools/dump_primes.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright 2011 Yesudeep Mangalapilly <yesudeep@gmail.com>
-# Copyright 2012 Google, Inc.
+# Copyright 2012 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
